0270a29069a9d752926e7c1cbd67267a3cfbd3d6
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-23 19:52:00 +0530

    Use let and const

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ console.error("This is an error");
 
 // write a warning to the console
 console.warn("This is a warning");
+
+// difference between let and const
+let score = 20;
+score = 50;
+
+console.log(score);
+
+const age = 25;
+age = 26;
